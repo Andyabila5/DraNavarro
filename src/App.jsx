a5b535970245546609724c56823b388bf266e63b
andyabila5 <andyabila5@gmail.com>
2025-06-22 00:17:24 -0600

    Arreglando Problemas

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'; 
-import About from './pages/About'; 
-import Services from './pages/Services'; 
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/servicios" element={<Services />} />
-      <Route path="/procedimientos" element={<div>Página de Procedimientos</div>} />
-      <Route path="/blog" element={<div>Página de Blog</div>} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/servicios" element={<Services />} />
+      </Routes>
+    </>
   );
 }
 
